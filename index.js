9de3b58b59570f4db1db548afa9a02d8a4fcd18c
Luke Cross <luke.cross25@gmail.com>
2022-09-28 12:02:09 -0600

    commit tree 0992c3e65dc79c28f0f1de379bd2c2581c7b709a

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+  function destructivelyAppendCat(string) {
+cats.push(string);
+return cats;
+}
+destructivelyAppendCat('Ralph')
+
+    function destructivelyPrependCat(string) {
+cats.unshift(string);
+return cats;
+}
+destructivelyPrependCat('Bob') 
+    function destructivelyRemoveLastCat() {
+    cats.pop()
+        return cats
+}
+    function destructivelyRemoveFirstCat() {
+        cats.shift()
+        return cats
+    }
+    function appendCat(string) {
+        appendCat = cats.slice();
+        appendCat.push(string);
+        return appendCat
+    }
+    appendCat("Broom")
+    function prependCat(string) {
+        prependCat = cats.slice();
+        prependCat.unshift(string);
+        return prependCat
+    }
+    function removeLastCat() {
+        removeLastCat = cats.slice(0, -1);
+        return removeLastCat
+    }
+
+    function removeFirstCat() {
+       removeFirstCat = cats.slice(1);
+       return removeFirstCat
+    }
